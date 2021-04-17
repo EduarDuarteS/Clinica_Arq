608b8e3b1abc2a8ee91b857fb09bea7d785067b6
ma.hoyosr <ma.hoyosr@uniandes.edu.co>
2021-04-16 23:28:11 -0500

    id confirmacion salida

--- nextjs/pages/confirmacion.js
+++ nextjs/pages/confirmacion.js
@@ -1,9 +1,18 @@
 import { Button } from "react-bootstrap";
 
 const confirmacion = () => {
+  const makeIdSalida = () => {
+    let result = [];
+    const characters = "1234567890";
+    for (let i = 0; i < 5; i++) {
+      result.push(characters.charAt(Math.floor(Math.random() * 10)));
+    }
+    return result.join("");
+  };
   return (
     <>
       <h2>El paciente fue dado de alta</h2>
+      <h3>Id confirmacion: {makeIdSalida()}</h3>
       <Button href="/alta1">Dar de alta otro paciente</Button>
     </>
   );
